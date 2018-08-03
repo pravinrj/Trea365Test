6a189a9a4b8aee0a39de335ea1865480b17281d5
atultatke786 <atultatke786@gmail.com>
2018-08-03 17:58:44 +0530

    Home Page changes by Atul

--- src/main/java/com/erp/qa/pages/HomePage.java
+++ src/main/java/com/erp/qa/pages/HomePage.java
@@ -1,13 +1,38 @@
 package com.erp.qa.pages;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.erp.qa.base.TestBase;
+/*import com.erp.qa.pages.Accounting.InvoicePage;
+import com.erp.qa.pages.Accounting.PaymentSchedulesPage;
 import com.erp.qa.pages.Admin.BusinessNaturePage;
-
+import com.erp.qa.pages.Admin.ItemCategoriesPage;
+import com.erp.qa.pages.Admin.ItemDefinitionPage;
+import com.erp.qa.pages.Admin.LabelsPage;
+import com.erp.qa.pages.Admin.LocationsPage;
+import com.erp.qa.pages.Admin.MFGPage;
+import com.erp.qa.pages.Admin.PermissionsPage;
+import com.erp.qa.pages.Admin.SuppliersPage;
+import com.erp.qa.pages.CRM.CRMPage;
+import com.erp.qa.pages.HR.EmployeesPage;
+import com.erp.qa.pages.HR.SubContractorPage;
+import com.erp.qa.pages.HR.TimesheetsPage;
+import com.erp.qa.pages.Inventory.AssetsPage;
+import com.erp.qa.pages.Inventory.AuditPage;
+import com.erp.qa.pages.Inventory.MaintenancePage;
+import com.erp.qa.pages.Inventory.MaterialPage;
+import com.erp.qa.pages.Inventory.Productspage;
+import com.erp.qa.pages.Inventory.PurchaseOrdersPage;
+import com.erp.qa.pages.Inventory.RecievingSlipsPage;
+import com.erp.qa.pages.Messaging.MessagingPage;
+import com.erp.qa.pages.WorkFlow.ProjectEstimatorPage;
+import com.erp.qa.pages.WorkFlow.SchedulingPage;
+import com.erp.qa.pages.WorkFlow.WorkOrdersPage;
+**/
 public class HomePage extends TestBase {
 
 	@FindBy(xpath = "/html/body/trea-app-root/ng-component/div/header/nav/div/ul/li[1]/a/span")
@@ -17,16 +42,105 @@ public class HomePage extends TestBase {
 	@FindBy(xpath = "/html/body/trea-app-root/ng-component/div/div[2]/section/app-summary/div/div[1]")
 	@CacheLookup
 	WebElement HomePageTitle;
-		
+	
+	
+
 	@FindBy(xpath = "//*[@id='leftside-navigation']/ul/li[2]/a/span[1]")
 	WebElement AdminTab;
 	
 	@FindBy(xpath = "//*[@id='leftside-navigation']/ul/li[2]/ul/li[1]/a")
+	
 	WebElement NatureofBusinessLink;
 	
-	@FindBy(xpath="//*[@id='leftside-navigation']/ul/li[2]/ul/li[4]/a")
-	WebElement MfgPartLink;
+	@FindBy(xpath = "//*[@id='leftside-navigation']/ul/li[2]/ul/li[2]/a")
+	WebElement LocationsLink;
 	
+	@FindBy(xpath="//*[@id='leftside-navigation']/ul/li[2]/ul/li[4]/a")
+	WebElement MFGLink;
+	
+	@FindBy(xpath="//*[@id='leftside-navigation']/ul/li[2]/ul/li[5]/a")
+	WebElement ItemDefinitionLink;
+	
+	@FindBy(xpath="//*[@id='leftside-navigation']/ul/li[2]/ul/li[3]/a")
+	WebElement ItemCategoriesLink;
+	@FindBy(xpath="//*[@id='leftside-navigation']/ul/li[2]/ul/li[6]/a")
+	WebElement SuppliersLink;
+	@FindBy(xpath="//*[@id='leftside-navigation']/ul/li[2]/ul/li[7]/a")
+	WebElement PermissionsLink ;
+	@FindBy(xpath="//*[@id='leftside-navigation']/ul/li[2]/ul/li[8]/a")
+	WebElement LabelsLink;
+	@FindBy(xpath ="//*[@id=\"leftside-navigation\"]/ul/li[3]/a/span[1]")
+	WebElement InventoryTab ;
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[3]/ul/li[1]/a")
+	WebElement ProductsLink;
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[3]/ul/li[2]/a")
+	WebElement AssetsLink;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[3]/ul/li[3]/a")
+	WebElement MaterialLink;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[3]/ul/li[4]/a")
+	WebElement MaintenanceLink ;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[3]/ul/li[5]/a")
+	WebElement PurchaseOrdersLink;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[3]/ul/li[6]/a")
+	WebElement RecievingSlipLink;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[3]/ul/li[7]/a")
+	WebElement AuditLink ;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[4]/a/span[1]")
+	WebElement WorkflowTab;
+	
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[4]/ul/li[1]/a")
+	WebElement ProjectEstimatorLink;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[4]/ul/li[2]/a")
+	WebElement WorkOrdersLink;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[4]/ul/li[3]/a")
+	WebElement SchedulingLink;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[5]/a/span")
+	
+	
+	WebElement CRMLink;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[6]/a/span[1]")
+	WebElement HRTab;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[6]/ul/li[1]/a")
+	WebElement EmployeesLink;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[6]/ul/li[2]/a")
+	WebElement SubContractorLink;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[6]/ul/li[3]/a")
+	WebElement TimesheetsLink;
+	
+	@FindBy(xpath ="//*[@id=\"leftside-navigation\"]/ul/li[7]/a/span[1]")
+	WebElement AccountingTab;
+	
+	@FindBy(xpath ="//*[@id='leftside-navigation']/ul/li[7]/ul/li[1]/a")
+	WebElement PaymentSchedulesLink;
+
+	@FindBy(xpath="//*[@id='leftside-navigation']/ul/li[7]/ul/li[2]/a")
+	WebElement InvoiceLink;
+	
+	@FindBy(xpath="//*[@id='leftside-navigation']/ul/li[8]/a/span")
+	WebElement MessagingLink;
+
+	//private WebElement BusinessOfNatureLink;
+	
+//	@FindBy(xpath = "//a[contains(text(),'Deals')]")
+//	WebElement dealsLink;
+//
+//	@FindBy(xpath = "//a[contains(text(),'Tasks')]")
+//	WebElement tasksLink;
+
 	// Initializing the Page Objects:
 	public HomePage() {
 		PageFactory.initElements(driver, this);
@@ -46,13 +160,336 @@ public class HomePage extends TestBase {
 		AdminTab.click();
 	}
 	
-	public BusinessNaturePage clickOnNatureofBusinessLink(){
+	
+	
+// Get Url For NatureofBusiness page
+	
+	public String GetNatureofBusinessPageURL()
+	{
+		NatureofBusinessLink.click();
+		return driver.getCurrentUrl();
+		
+	}
+	
+	//Get url of Location page
+	public String GetLocationsPageURL()
+	{
+		LocationsLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//Manufacturer page
+	public String GetMFGPageURL()
+	{
+		MFGLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//Item definition Page
+	
+	public String GetItemDefinitionPageURL()
+	{
+		ItemDefinitionLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//Item Categories page
+	
+	public String GetItemCategoriesPageURL()
+	{
+		ItemCategoriesLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//Suppliers page
+	public String GetSuppliersPageURL()
+	{
+		SuppliersLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//PermissionsPage
+	
+	public String GetPermissionsPageURL()
+	{
+		PermissionsLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//Labels Page
+	public String GetLabelsPageURL()
+	{
+		LabelsLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//Productspage
+	
+	
+	public String GetProductsPageURL()
+	{
+		ProductsLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//AssetsPage
+	public String GetAssetsPageURL()
+	{
+		AssetsLink.click();
+		return driver.getCurrentUrl();
+	}
+	//Material page
+	public String GetMaterialPageURL()
+	{
+		MaterialLink.click();
+		return driver.getCurrentUrl();
+	}
+	// Maintenance page
+	public String GetMaintenancePageURL()
+	{
+		MaintenanceLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//PurchaseOrdersPage
+	public String GetPurchaseOrdersPageURL()
+	{
+		PurchaseOrdersLink.click();
+		return driver.getCurrentUrl();
+	}
+	//RecievingSlipsPage
+	
+	public String GetRecievingSlipsPageURL()
+	{
+		 RecievingSlipLink.click();
+		return driver.getCurrentUrl();
+	}
+	//AuditPage
+	
+	public String GetAuditPageURL()
+	{
+		AuditLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//ProjectEstimator
+	public String GetProjectEstimatorPageURL()
+	{
+		ProjectEstimatorLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//WorkOrders
+	public String GetWorkOrdersPageURL()
+	{
+		WorkOrdersLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//Scheduling
+	public String GetSchedulingsPageURL()
+	{
+		SchedulingLink.click();
+		return driver.getCurrentUrl();
+	}
+	//CRMPage
+	public String GetCRMPageURL()
+	{
+		CRMLink.click();
+		return driver.getCurrentUrl();
+	}
+
+	//Employee 
+	public String GetEmployeesPageURL()
+	{
+		EmployeesLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//SubContractor
+	public String GetSubContractorPageURL()
+	{
+		SubContractorLink.click();
+		return driver.getCurrentUrl();
+	}
+		//Timesheets
+	public String GetTimesheetsPageURL()
+	{
+		TimesheetsLink.click();
+		return driver.getCurrentUrl();
+	}
+	
+	//PaymentSchedules  
+	public String GetPaymentSchedulesPageURL()
+	{
+		PaymentSchedulesLink.click();
+		return driver.getCurrentUrl();
+	}
+	//Invoice
+	
+	public String GetInvoicePageURL()
+	{
+		InvoiceLink.click();
+		return driver.getCurrentUrl();
+	}
+	//Messaging
+	public String GetMessagingPageURL()
+	{
+		MessagingLink.click();
+		return driver.getCurrentUrl();
+	}
+}
+	
+	/*public BusinessNaturePage clickOnNatureofBusinessLink() {
 		NatureofBusinessLink.click();
 		return new BusinessNaturePage();
 	}
-	/*public void ClickOnNatureofBusinessTab()
-	{
-		NatureofBusinessLink.click();
-	}*/
 	
+	public LocationsPage clickOnLocationsLink() {
+		LocationsLink.click();
+		return new LocationsPage();
+	}
+	
+	public MFGPage clickOnMFGLink() {
+		
+		MFGLink.click();
+		return new MFGPage();
+	}
+	public ItemDefinitionPage clickOnItemDefinitionLink() {
+	
+		ItemDefinitionLink.click();
+		return new ItemDefinitionPage ();
+	}
+	public ItemCategoriesPage clickOnItemCategoriesLink() {
+		
+		ItemCategoriesLink.click();
+		return new ItemCategoriesPage ();
+	}
+	public SuppliersPage clickOnSuppliersLink() {
+		SuppliersLink.click();
+		return new SuppliersPage();
+	}
+	public PermissionsPage clickonPermissionsLink() {
+		PermissionsLink.click();
+		return new PermissionsPage();
+	}
+	public LabelsPage clickOnLabelsLink() {
+		LabelsLink.click();
+		return new LabelsPage();
+	}
+	
+	public void clickOnInventoryTab()
+	{
+		InventoryTab.click();
+	}
+	
+	public Productspage clickOnProductsLink() {
+		ProductsLink.click();
+		return new Productspage();
+	}
+	
+	public AssetsPage clickOnAssetsLink() {
+		AssetsLink.click();
+		return new AssetsPage();
+	}
+
+	public MaterialPage clickOnMaterialLink() {
+		
+		MaterialLink.click();
+		return new MaterialPage();
+		
+	}
+	
+public MaintenancePage clickOnMaintenanceLink() {
+		
+	MaintenanceLink.click();
+		return new MaintenancePage();
+		
+	}
+	
+	public PurchaseOrdersPage clickOnLink(){
+		PurchaseOrdersLink.click();
+		return new PurchaseOrdersPage();
+	}
+	
+	public RecievingSlipsPage clickOnRecievingSlipsLink(){
+		 RecievingSlipLink.click();
+		return new  RecievingSlipsPage();
+		}
+	
+	public AuditPage clickonAuditLink() {
+		AuditLink.click();
+		return new AuditPage();
+	}
+	
+	public void clickOnWorkflowTab()
+	{
+		WorkflowTab.click();
+	}
+	public ProjectEstimatorPage clickOnProjectEstimatorLink() {
+		ProjectEstimatorLink.click();
+		return new ProjectEstimatorPage();
+	}
+	public WorkOrdersPage clickOnWorkOrdersLink() {
+		WorkOrdersLink.click();
+		return new WorkOrdersPage();
+	}
+	public SchedulingPage clickOnSchedulingLink() {
+		SchedulingLink.click();
+		return new SchedulingPage();
+	}
+	
+	public CRMPage clickOnCRMLink() {
+	CRMLink.click();
+	return new CRMPage();
+	}
+	public void clickOnHRTab()
+	{
+		HRTab.click();
+	}
+	public EmployeesPage clickOnEmployeesLink() {
+		EmployeesLink.click();
+		return new EmployeesPage();
+	}
+	public SubContractorPage clickOnSubContractorLink() {
+		SubContractorLink.click();
+		return new SubContractorPage();
+		
+	}
+	public TimesheetsPage clickOnTimesheetsLink() {
+		TimesheetsLink.click();
+		return new TimesheetsPage();
+		
+	}
+	public void clickOnAccountingTab()
+	{
+		AccountingTab.click();
+		//
+	}
+	public PaymentSchedulesPage clickOnPaymentSchedulesLink() {
+		PaymentSchedulesLink.click();
+		return new PaymentSchedulesPage();
+		
+	}
+	public InvoicePage clickOnInvoiceLink() {
+		InvoiceLink.click();
+		return new InvoicePage();
+		
+	}
+	public MessagingPage clickOnMessagingLink() {
+		MessagingLink.click();
+		return new MessagingPage();
 }
+
+	
+} **/
+
+	
+	
+	
+	
+	
+	
+	
+
+
--- src/test/java/com/erp/qa/testcases/HomePageTest.java
+++ src/test/java/com/erp/qa/testcases/HomePageTest.java
@@ -47,16 +47,227 @@ public class HomePageTest extends TestBase {
 	}
 	
 	@Test(priority=3)
-	public void verifyNatureOfBusinessLinkTest(){
+	public void verifyNatureOfBusinessURLTest() throws InterruptedException{
 		//testUtil.switchToFrame();
 		homePage.clickOnAdminTab();
-		businessNaturePage = homePage.clickOnNatureofBusinessLink();
+		Thread.sleep(3000);
+		String url= homePage.GetNatureofBusinessPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/business-nature", "URL not match");
+		
 	}
+	
+	@Test(priority=4)
+	public void verifyLocationsURLTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnAdminTab();
+		Thread.sleep(3000);
+		String url= homePage.GetLocationsPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/location-list", "URL not match");
+		}
+	@Test(priority=5)
+	public void verifyMFGURLTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnAdminTab();
+		Thread.sleep(3000);
+		String url= homePage.GetMFGPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/manufacturer", "URL not match");
+		}
+	@Test(priority=6)
+	public void verifyItemDefinitionTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnAdminTab();
+		Thread.sleep(3000);
+		String url= homePage.GetItemDefinitionPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/manufacturer-part", "URL not match");
+		}
+	@Test(priority=7)
+	public void verifyItemCategoriesTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnAdminTab();
+		Thread.sleep(3000);
+		String url= homePage.GetItemCategoriesPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/item-classes", "URL not match");
+		}
+	@Test(priority=8)
+	public void verifySuppliersTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnAdminTab();
+		Thread.sleep(3000);
+		String url= homePage.GetSuppliersPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/supplier-list", "URL not match");
+		}
+	@Test(priority=9)
+	public void verifyPermissionsTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnAdminTab();
+		Thread.sleep(3000);
+		String url= homePage.GetPermissionsPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/role-list", "URL not match");
+		}
+		
+	@Test(priority=10)
+	public void verifyLabelsTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnAdminTab();
+		Thread.sleep(3000);
+		String url= homePage.GetLabelsPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/print-label", "URL not match");
+		}
+	/*@Test(priority=11)
+	public void verifyProductsTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnInventoryTab();
+		Thread.sleep(3000);
+		String url= homePage.GetProductsPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/inventory/product-list", "URL not match");
+		}
+	@Test(priority=12)
+	public void verifyAssetsTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnInventoryTab();
+		Thread.sleep(3000);
+		String url= homePage.GetAssetsPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/inventory/asset-list", "URL not match");
+		}
+	@Test(priority=13)
+	public void verifyMaterialTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnInventoryTab();
+		Thread.sleep(3000);
+		String url= homePage.GetMaterialPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/inventory/material-list", "URL not match");
+		}
+	@Test(priority=14)
+	public void verifyMaintenanceTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnInventoryTab();
+		Thread.sleep(3000);
+		String url= homePage.GetMaintenancePageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/inventory/maintenance-list", "URL not match");
+		}
+	@Test(priority=15)
+	public void verifyPurchaseOrderTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnInventoryTab();
+		Thread.sleep(3000);
+		String url= homePage.GetPurchaseOrdersPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/inventory/purchase-order-list", "URL not match");
+		}
+	
+	@Test(priority=16)
+	public void verifyRecievingSlipsTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnInventoryTab();
+		Thread.sleep(3000);
+		String url= homePage.GetRecievingSlipsPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/inventory/receiving-slips-list", "URL not match");
+		}
+	
+	@Test(priority=17)
+	public void verifyAuditTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnInventoryTab();
+		Thread.sleep(3000);
+		String url= homePage.GetAuditPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/inventory/audit-list", "URL not match");
+		}
+	
+	//WorkflowTab  WorkOrders Scheduling
+	
+	@Test(priority=18)
+	public void verifyProjectEstimatorTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnWorkflowTab();
+		Thread.sleep(3000);
+		String url= homePage.GetProjectEstimatorPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/quotation-list", "URL not match");
+		}
+	@Test(priority=19)
+	public void verifyWorkOrdersTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnWorkflowTab();
+		Thread.sleep(3000);
+		String url= homePage.GetWorkOrdersPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/work-order-list", "URL not match");
+		}
+	
+	@Test(priority=20)
+	public void verifySchedulingTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnWorkflowTab();
+		Thread.sleep(3000);
+		String url= homePage.GetSchedulingsPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/schedule-list", "URL not match");
+		}
+	@Test(priority=21)
+	public void verifyCRMTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnCRMLink();
+		Thread.sleep(4000);
+	
+		
+		String url= homePage.GetCRMPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/crm/csa/client-list", "URL not match");
+		}
+	//Employees SubContractor Timesheets HRTab
+	@Test(priority=22)
+	public void verifyEmployeesTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnHRTab();
+		Thread.sleep(3000);
+		String url= homePage.GetEmployeesPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/employee-list", "URL not match");
+		}
+	@Test(priority=23)
+	public void verifySubContractorTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnHRTab();
+		Thread.sleep(3000);
+		String url= homePage.GetSubContractorPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/sub-contractor-list", "URL not match");
+		}
+	@Test(priority=24)
+	public void verifyTimesheetsTest() throws InterruptedException{
+		//testUtil.switchToFrame();
+		homePage.clickOnHRTab();
+		Thread.sleep(3000);
+		String url= homePage.GetTimesheetsPageURL();
+		Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/timesheet-list", "URL not match");
+		}
+	
+	
+		@Test(priority=25)
+		public void verifyPaymentSchedulesTest() throws InterruptedException{
+			//testUtil.switchToFrame();
+			homePage.clickOnAccountingTab();
+			Thread.sleep(3000);
+			String url= homePage.GetPaymentSchedulesPageURL();
+			Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/acc-receivables", "URL not match");
+			}
+		@Test(priority=26)
+		public void verifyInvoiceTest() throws InterruptedException{
+			//testUtil.switchToFrame();
+			homePage.clickOnAccountingTab();
+			Thread.sleep(3000);
+			String url= homePage.GetInvoicePageURL();
+			Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/create-invoice", "URL not match");
+			}
+		//MessagingPage
+		@Test(priority=27)
+		public void verifyMessagingTest() throws InterruptedException{
+			//testUtil.switchToFrame();
+			homePage.clickOnMessagingLink();
+			Thread.sleep(4000);
+		
+			
+			String url= homePage.GetMessagingPageURL();
+			Assert.assertEquals(url, "https://www.trea365.com/steam/#/csa/messaging", "URL not match");
+			}
+		**/
 		
 	@AfterMethod
 	public void tearDown(){
 		driver.quit();
 	}
-	
-
 }
+	
